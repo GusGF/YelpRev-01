4d991006cb61e92ba80094d214ac9b4d512e3dff
Fergus <intel4004u@gmail.com>
2022-10-23 20:10:03 +0100

    2. Campground Model Basics (Mongoose)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const Campground = require('./models/campground')
 const app = express()
 const port = 3003
 
@@ -13,8 +14,24 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelprev-01', {
   useUnifiedTopology: true
 });
 
+const dbConn = mongoose.connection
+dbConn.on('error', console.error.bind(console, 'connection error:'))
+dbConn.once('open', () => { console.log("We are connected") })
+
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/makecampground', (req, res) => {
+  const myNewCampground = new Campground({
+    title: 'Ballybough Campsite',
+    price: 1.00,
+    description: 'Full of knackers',
+    location: 'Dublin',
+    image: 'https://source.unsplash.com/collection/483251'
+  })
+  console.log(myNewCampground)
+  res.send(myNewCampground)
 })
 
 app.listen(port, (req, res) => {
--- models/campground.js
+++ models/campground.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const campgroundSchema = new mongoose({
+const campgroundSchema = new mongoose.Schema({
   title: String,
   price: Number,
   description: String,
   location: String,
   image: String
 })
-module.exports = mongoose.model('Campground', 'campgroundSchema')
+module.exports = mongoose.model('Campground', campgroundSchema)
